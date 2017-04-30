c5d2fad1790d52efa4b54073a99a1bb7a8fb7753
John Van Schultz <jvansch1@binghamton.edu>
2017-04-30 18:35:05 -0400

    Connect database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const path = require('path')
 const mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1/AddressBook')
 
 app.use('/static', express.static(path.resolve(__dirname, 'public')))
 app.set('view engine', 'pug')
