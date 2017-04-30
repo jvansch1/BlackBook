88331a92fdd39b266e66b1819b3f50ce823a162f
John Van Schultz <jvansch1@binghamton.edu>
2017-04-30 18:03:48 -0400

    Create entry point for React

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-app.listen(port)
+const path = require('path')
 
-app.use('/static', express.static(__dirname + 'public'))
+app.use('/static', express.static(path.resolve(__dirname, 'public')))
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+app.listen(port)
