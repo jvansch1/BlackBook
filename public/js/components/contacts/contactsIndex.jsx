335b3a529d0ebc4a9949da01dd8f297d49d6d2be
John Van Schultz <jvansch1@binghamton.edu>
2017-05-01 15:25:48 -0400

    Add modal to contacts index page

--- public/js/components/contacts/contactsIndex.jsx
+++ public/js/components/contacts/contactsIndex.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import Header from '../header/header.jsx'
+import Modal from 'react-modal'
 
 export default class contactsIndex extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      modalIsOpen: false
+    }
+
+    this.openModal = this.openModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
   }
 
   componentDidMount() {
     this.props.fetchContacts()
   }
 
+  openModal() {
+    this.setState({modalIsOpen: true})
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false})
+  }
+
   render() {
-    console.log(this.props.contacts)
     return (
       <div>
         <Header />
@@ -19,9 +33,9 @@ export default class contactsIndex extends React.Component {
           <h1>Index</h1>
           <ul>
             {
-              this.props.contacts.map(contact => {
+              this.props.contacts.map((contact, idx) => {
                 return (
-                  <li>
+                  <li className='contact' key={contact._id}>
                     <p>{contact.name}</p>
                     <p>{contact.address}</p>
                   </li>
@@ -30,6 +44,16 @@ export default class contactsIndex extends React.Component {
             }
           </ul>
         </div>
+        <button onClick={this.openModal}>Open Modal</button>
+        <Modal isOpen={this.state.modalIsOpen} contentLabel='Example'>
+          <button onClick={this.closeModal}>Close</button>
+          <form id='contacts-form'>
+            Name
+            <input type='text'/>
+            Address
+            <input type='text'/>
+          </form>
+        </Modal>
       </div>
     )
   }
--- public/js/reducers/contactsReducer.js
+++ public/js/reducers/contactsReducer.js
@@ -1,7 +1,6 @@
 import { RECEIVE_CONTACTS } from '../actions/contactActions.js'
 
 const contactsReducer = (state = {}, action) => {
-  console.log(action)
   switch (action.type) {
     case RECEIVE_CONTACTS:
       return action.contacts
